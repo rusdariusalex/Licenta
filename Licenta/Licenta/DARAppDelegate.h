02ca6df5ac1ffaeb02672d75dbeca385552e772d
Rus Darius <rusdariusalex@gmail.com>
2014-04-29 17:51:56 +0300

    Empty Project

---
+++ Licenta/Licenta/DARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DARAppDelegate.h
+//  Licenta
+//
+//  Created by darius on 29/04/14.
+//  Copyright (c) 2014 Darius Rus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Licenta/Licenta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Licenta
+//
+//  Created by darius on 29/04/14.
+//  Copyright (c) 2014 Darius Rus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DARAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DARAppDelegate class]));
+    }
+}
